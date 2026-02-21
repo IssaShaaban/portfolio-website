353ffd4fede52f75458a7018bd6b8bf5b1731b0b
shaaban <issashab44@gmail.com>
2026-02-21 20:25:01 +0000

    Added new styles, re-worked cv page, added content to projects page

--- navbar.js
+++ navbar.js
@@ -2,8 +2,7 @@ class MyNavbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <nav class="navbar">
-            <div class="logo"></div>
-            <ul class="nav-links">
+            <div class="logo">>./loadIssaShaabanPortfolio()</div> <ul class="nav-links">
                 <li><a href="index.html">Home</a></li>
                 <li><a href="cv.html">CV</a></li>
                 <li><a href="projects.html">Projects</a></li>
@@ -13,3 +12,27 @@ class MyNavbar extends HTMLElement {
     }
 }
 customElements.define('main-navbar', MyNavbar);
+
+function switchProject(event, projectId) {
+    const views = document.querySelectorAll('.project-view');
+    views.forEach(view => view.classList.remove('active'));
+
+    const tabs = document.querySelectorAll('.tab-link');
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    document.getElementById(projectId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const projectToOpen = params.get('project');
+
+    if (projectToOpen) {
+        const targetBtn = document.querySelector(`button[onclick*="'${projectToOpen}'"]`);
+
+        if (targetBtn) {
+            targetBtn.click();
+        }
+    }
+});
